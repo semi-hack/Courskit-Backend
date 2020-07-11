748c7326cf87b89c4e9c8efff88ce3ed8f29576d
semi-hack <asemiloore@gmail.com>
2020-07-11 10:08:54 +0100

    REFACTOR

--- controller/class_controller.js
+++ controller/class_controller.js
@@ -1,37 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Class = require("../models/class");
+const { Class } = require("../models/class");
 
-// CREATE a CLASS
-const createClass = async (req, res) => {
+// create a class
+const createclass = async (req, res) => {
     const { name, Course, AcademicPeriod, Meeting, Population, UnavailableRooms } = req.body;
-  
-    try {
-      const existingclass = await Class.findOne({ name: req.body.name }).exec();
-      if (existingclass) {
-          return res.status(401).json({
-            message: "name already taken",
-          });
-      }
 
-      const class = new Class({
-          name,
-          Course,
-          AcademicPeriod,
-          Meeting,
-          Population,
-          UnavailableRooms
-      });
-     
-      await class.save()
-      return res.json({
-          success: true,
-          data: room
+    try {
+        const existingclass = await Class.findOne({ name: req.body.name }).exec();
+        if (existingclass) {
+            return res.status(401).json({
+                message: "name already taken",
+            });
+        }
+
+        const Xclass = new Class({
+            name,
+            Course,
+            AcademicPeriod,
+            Meeting,
+            Population,
+            UnavailableRooms
+        });
+        await Xclass.save()
+        return res.json({
+            success: true,
+            data: class
       });
     } catch (error) {
-      return res.status(500).json({
-          error: 'There was an error.',
-          success: false
-      });
+        return res.status(500).json({
+            error: 'There was an error.',
+            success: false
+        });
     }
 }
