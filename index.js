0c0ff78ff5b2cf723433ffe4019c5b32b7bde151
semi-hack <asemiloore@gmail.com>
2020-07-27 09:04:43 +0100

    new commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = require('./routes/routes');
 
 const app = express()
 connectDB();
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
