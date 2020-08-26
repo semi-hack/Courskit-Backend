517277c2c068d4f940604cac2db2beff340dc39b
semi-hack <asemiloore@gmail.com>
2020-08-26 19:56:21 +0100

    fix profile reset password

--- routes/routes.js
+++ routes/routes.js
@@ -50,7 +50,7 @@ router.patch('/user/register/course', userController.RegisterCourse);
 router.route('/user/image').patch(parser.single('image'), userController.UpdateUserImage)
 router.post('/forgotPassword', userController.forgotPassword);
 router.post('/resetPassword', user.resetPassword);
-router.post('/profile/resetPassword', user.resetPasswordwithOldPassword);
+router.patch('/profile/resetPassword', user.resetPasswordwithOldPassword);
 router.get('/user/getCourse/id', courseController.GetCourseById)
 router.get('/user/getCourse/search', courseController.search);
 router.get('/user/level/getCourse', courseController.GetCourseByLevel)
