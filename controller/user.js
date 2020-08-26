502a032261a10d54e31cacd8194999150af15a58
semi-hack <asemiloore@gmail.com>
2020-08-26 05:01:34 +0100

    Refactor getUser
    populated the course field of user being returned

--- controller/user.js
+++ controller/user.js
@@ -5,9 +5,9 @@ const Course = require("../models/course");
 const jwt = require("jsonwebtoken");
 const { secret } = require("../config/helper");
 const { transporter } = require("../config/nodemailer");
-const { nanoid, customAlphabet } = require('nanoid');
+const { nanoid, customAlphabet } = require("nanoid");
 const bcrypt = require("bcrypt");
-var _ = require('lodash');
+var _ = require("lodash");
 const { nextTick } = require("process");
 const { error } = require("console");
 const { use } = require("bcrypt/promises");
@@ -100,8 +100,10 @@ const signup = async (req, res) => {
         success: false,
       });
     }
-    const course = await Course.find({ level: req.body.level}).populate('venue').populate('lecturer')
-    console.log(course)
+    const course = await Course.find({ level: req.body.level })
+      .populate("venue")
+      .populate("lecturer");
+    console.log(course);
 
     const user = new User({
       firstname,
@@ -112,7 +114,7 @@ const signup = async (req, res) => {
       matric,
       password,
       role,
-      courses: course
+      courses: course,
     });
 
     await user.save();
@@ -128,63 +130,62 @@ const signup = async (req, res) => {
   }
 };
 
-
 const forgotPassword = (req, res) => {
-    const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 5)
-    const token = nanoid();
-    User.findOne({ email: req.body.email }, (err, user) => {
-      if (!user) {
-        return res.status(422).json("user does not exist");
-      }
-      transporter.sendMail({
-        to: user.email,
-        from: "testd20202@gmail.com",
-        subject: "Password-Reset",
-        html: `<p><h1>Password Reset</h1> <p>
+  const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+  const token = nanoid();
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.status(422).json("user does not exist");
+    }
+    transporter.sendMail({
+      to: user.email,
+      from: "testd20202@gmail.com",
+      subject: "Password-Reset",
+      html: `<p><h1>Password Reset</h1> <p>
             <h4> Your reset code is:   ${token}</h4><br>
             <h5>Use this code to reset your password. This code should not be shared</h5><br>`,
-      });
-      console.log(token)
-      res.json({ message: "check mail" });
-      return user.updateOne({ resetToken: token }, (err, success) => {
-        if (err) {
-          return res.status(400).json({ error: "USER DOESNT EXIST" });
-        }
-      });
     });
+    console.log(token);
+    res.json({ message: "check mail" });
+    return user.updateOne({ resetToken: token }, (err, success) => {
+      if (err) {
+        return res.status(400).json({ error: "USER DOESNT EXIST" });
+      }
+    });
+  });
 };
 
 const resetPassword = (req, res) => {
-  const { resetToken, newpassword } = req.body
+  const { resetToken, newpassword } = req.body;
   User.findOne({ resetToken: req.body.resetToken }, (err, user) => {
-    if(!user) {
-      return res.status(422).json({ error: "wrong token" })
+    if (!user) {
+      return res.status(422).json({ error: "wrong token" });
     }
-    console.log(user)
-    
+    console.log(user);
+
     const obj = {
       password: newpassword,
-      resetToken: ''
-    }
+      resetToken: "",
+    };
 
     user = _.extend(user, obj);
     user.save((err, result) => {
-      if(err) {
-        return res.status(400).json({error: "reset password error"});
+      if (err) {
+        return res.status(400).json({ error: "reset password error" });
       } else {
-        return res.status(200).json({message: "password has been changed"})
+        return res.status(200).json({ message: "password has been changed" });
       }
     });
   });
 };
 
 const resetPasswordwithOldPassword = (req, res) => {
-  const { _id, oldPassword, newpassword } = req.body
+  const { _id, oldPassword, newpassword } = req.body;
   User.findOne({ _id: req.body._id }, (err, user) => {
-    if(!user) {
-      return res.status(422).json({ error: "wrong token" })
+    if (!user) {
+      return res.status(422).json({ error: "wrong token" });
     }
-    
+
     user.comparePassword(req.body.password, (err, match) => {
       if (!match) {
         return response
@@ -192,104 +193,127 @@ const resetPasswordwithOldPassword = (req, res) => {
           .send({ message: "The password is invalid" });
       }
     });
-    
+
     const obj = {
       password: newpassword,
-    }
+    };
 
     user = _.extend(user, obj);
     user.save((err, result) => {
-      if(err) {
-        return res.status(400).json({error: "reset password error"});
+      if (err) {
+        return res.status(400).json({ error: "reset password error" });
       } else {
-        return res.status(200).json({message: "password has been changed"})
+        return res.status(200).json({ message: "password has been changed" });
       }
     });
   });
 };
 
+const getUser = async (req, res) => {
+  try {
+    const token = req.headers.token;
 
-const getUser = (req, res) => {
-  const token = req.headers.token;
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err)
-      return res.status(401).json({
-        title: "unauthorized",
-      });
-    User.findOne({ _id: decoded._id }, (err, user) => {
-      if (err) return console.log(err);
-      return res.status(200).json({
-        title: "user granted",
-        user,
-      });
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findOne({ _id: decoded._id }).populate({
+      path: "courses",
+      populate: [
+        { path: "lecturer", model: "Lecturer" },
+        { path: "venue", model: "room" },
+      ],
     });
-  });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        title: "user granted",
+        message: user,
+      });
+    }
+  } catch (err) {
+    return res.status(401).json({
+      message: "unauthorized",
+    });
+  }
 };
 
 // Update User
 const UpdateUser = async (req, res) => {
-  const { _id } = req.headers
-  const UpdatedUser = await User.findByIdAndUpdate(req.headers._id, {$set: req.body});
+  const { _id } = req.headers;
+  const UpdatedUser = await User.findByIdAndUpdate(req.headers._id, {
+    $set: req.body,
+  });
   if (!UpdatedUser) {
-      res.status(400).json({
-          message: "failed to update"
-      });
+    res.status(400).json({
+      message: "failed to update",
+    });
   } else {
-      res.json({
-          success: true,
-          message: UpdatedUser
-      });
+    res.json({
+      success: true,
+      message: UpdatedUser,
+    });
   }
-
-}
+};
 
 const UpdateUserImage = async (req, res) => {
-  console.log(req.file)
-  const image = {}
-  const UpdatedUserImage = await User.findByIdAndUpdate(req.headers._id, {$set: {"image": req.file.path}});
+  console.log(req.file);
+  const image = {};
+  const UpdatedUserImage = await User.findByIdAndUpdate(req.headers._id, {
+    $set: { image: req.file.path },
+  });
   if (!UpdatedUserImage) {
-      res.status(400).json({
-          message: "failed"
-      });
+    res.status(400).json({
+      message: "failed",
+    });
   } else {
-      res.json({
-          success: true,
-          message: UpdatedUserImage
-      });
+    res.json({
+      success: true,
+      message: UpdatedUserImage,
+    });
   }
-
-}
+};
 
 // add courses to user
 const RegisterCourse = async (req, res) => {
-  const { _id } = req.headers
-  const UpdatedUser = await User.findByIdAndUpdate(req.headers._id, {$set: req.body}, {new: true}).populate({ 
-    path:'courses', populate: [{path: 'lecturer', model: 'Lecturer'}, {path: 'venue', model: 'room'}]
+  const { _id } = req.headers;
+  const UpdatedUser = await User.findByIdAndUpdate(
+    req.headers._id,
+    { $set: req.body },
+    { new: true }
+  ).populate({
+    path: "courses",
+    populate: [
+      { path: "lecturer", model: "Lecturer" },
+      { path: "venue", model: "room" },
+    ],
   });
   if (!UpdatedUser) {
-      res.status(400).json({
-          message: "failed to update"
-      });
+    res.status(400).json({
+      message: "failed to update",
+    });
   } else {
-      res.json({
-          success: true,
-          message: UpdatedUser
-      });
+    res.json({
+      success: true,
+      message: UpdatedUser,
+    });
   }
-}
+};
 
 const checkExistence = async (req, res) => {
-  const { email } = req.query
-  var mtu = /\w+@+mtu.edu.ng/
+  const { email } = req.query;
+  var mtu = /\w+@+mtu.edu.ng/;
   try {
-    if(mtu.test(req.query.email) == true ) {
+    if (mtu.test(req.query.email) == true) {
       return res.json({
         success: true,
-        data: "https://res.cloudinary.com/duqphnggn/image/upload/v1596850732/mtu_ljd7ex.jpg"
-      })
+        data:
+          "https://res.cloudinary.com/duqphnggn/image/upload/v1596850732/mtu_ljd7ex.jpg",
+      });
     } else {
       res.status(404).json({
-        success: false
+        success: false,
       });
     }
   } catch (err) {
@@ -298,6 +322,17 @@ const checkExistence = async (req, res) => {
       success: false,
     });
   }
-}
+};
 
-module.exports = { signup, login, forgotPassword, resetPassword, getUser, UpdateUser, UpdateUserImage, RegisterCourse, checkExistence, resetPasswordwithOldPassword };
+module.exports = {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  getUser,
+  UpdateUser,
+  UpdateUserImage,
+  RegisterCourse,
+  checkExistence,
+  resetPasswordwithOldPassword,
+};
