3ad80fd5431195885c7dc89eb2c77a738669770a
semi-hack <asemiloore@gmail.com>
2020-11-07 01:02:33 +0100

    timetable edit

--- controller/timetable.js
+++ controller/timetable.js
@@ -3,54 +3,32 @@ const axios = require("axios");
 const Course = require("../models/course");
 const Lecturer = require("../models/lecturer");
 const { Room } = require("../models/room");
-
-// classES
-// "Classes": [{
-//     "Subject": "Test Subject 1",
-//     "Type": "Theory",
-//     "Professor": "Vujosevic Dusan",
-//     "Groups": ["301", "302", "303", "304", "305", "306", "307", "308", "309", "309a"],
-//     "AllowedClassrooms": "n",
-//     "Length": "2"
-// }, {
-//     "Subject": "Test Subject 2",
-//     "Type": "Theory",
-//     "Professor": "Vujosevic Dusan",
-//     "Groups": ["301", "302", "303", "304", "305", "306", "307", "308", "309", "309a", "2s1", "2s2"],
-//     "AllowedClassrooms": "k",
-//     "Length": "2"
-// }
-
-//https://coursekit-timetable.herokuapp.com/generate/
+const { response } = require("express");
 
 const sendTimetabledata = async (req, res) => {
-  // const SUBJECT = await Course.aggregate([{ $project: { name: 1, _id: 0 } }]);
-  // const LT = await Room.aggregate([
-  //   { $match: { type: "LT" } },
-  //   { $project: { name: 1, _id: 0 } },
-  // ]);
-  // const NLT = await Room.aggregate([
-  //   { $match: { type: "NLT" } },
-  //   { $project: { name: 1, _id: 0 } },
-  // ]);
-  // const Professor = await Lecturer.aggregate([
-  //   { $project: { name: 1, _id: 0 } },
-  // ]);
-  // console.log(SUBJECT);
-  // console.log(LT, NLT);
-  // console.log(Professor);
   const data = req.body;
+  console.log(data);
 
-  async function makePostRequest() {
-    let res = await axios.get(
-      "https://coursekit-timetable.herokuapp.com/generate/",
-      data
-    );
+  var config = {
+    method: "get",
+    url: "https://coursekit-timetable.herokuapp.com/generate/",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
 
-    console.log(res.data);
-  }
-
-  makePostRequest();
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      const result = JSON.stringify(response.data)
+      return res.json({
+        data: result
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
 
 const receivedata = (req, res) => {
